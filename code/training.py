1b4045fb2af4018af40666b12d1c01c4b4ef58be
MennaHassan13 <mennahy13@gmail.com>
2025-12-13 18:04:12 +0200

    feat: training and evaluation

--- code/training.py
+++ code/training.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from pathlib import Path
 import keras
 from keras import layers, models
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from sklearn.metrics import classification_report
 
 np.random.seed(42)
 tf.random.set_seed(42)
@@ -126,3 +128,72 @@ class RoadSurfaceClassifier:
         self.model.summary()
 
         return self.model
+
+    def compile_model(self, learning_rate=0.001):
+        self.model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=learning_rate), # type: ignore
+            loss="categorical_crossentropy",
+            metrics=["accuracy", keras.metrics.Precision(), keras.metrics.Recall()],
+        )
+        print("\nModel compiled successfully!")
+
+    def train(self, train_ds, val_ds, epochs=40):
+        # Create model directory
+        model_dir = dirname / "../models"
+        model_dir.mkdir(exist_ok=True)
+
+        # Define callbacks
+        callbacks = [
+            EarlyStopping(
+                monitor="val_loss", patience=10, restore_best_weights=True, verbose=1
+            ),
+            ModelCheckpoint(
+                filepath=str(model_dir / "best_road_model.keras"),
+                monitor="val_accuracy",
+                save_best_only=True,
+                verbose=1,
+            ),
+            ReduceLROnPlateau(
+                monitor="val_loss", factor=0.5, patience=5, min_lr=1e-7, verbose=1
+            ),
+        ]
+
+        print(f"\nStarting training for {epochs} epochs...")
+
+        self.history = self.model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=epochs,
+            callbacks=callbacks,
+            verbose=1,
+        )
+
+        print("\nTraining completed!")
+        return self.history
+
+    def evaluate(self, test_ds):
+        print("\nEvaluating on test set...")
+        test_loss, test_acc, test_prec, test_recall = self.model.evaluate(
+            test_ds, 
+        )
+
+        print(f"Test Accuracy: {test_acc:.4f}")
+        print(f"Test Precision: {test_prec:.4f}")
+        print(f"Test Recall: {test_recall:.4f}")
+        print(f"Test Loss: {test_loss:.4f}")
+
+        # * only uses X part in test_ds
+        y_pred = self.model.predict(test_ds)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        y_test = np.concatenate([y.numpy() for x, y in test_ds], axis=0)
+        y_true_classes = np.argmax(y_test, axis=1)
+
+        print("\nClassification Report:")
+        print(
+            classification_report(
+                y_true_classes, y_pred_classes, target_names=self.class_names
+            )
+        )
+
+        return y_true_classes, y_pred_classes
+
