7b44fec9cf0fc2d1600e753782083466b978f399
Mohammed Ragab <123463070+MohammedRagab00@users.noreply.github.com>
2025-12-14 15:57:01 +0200

    feat: replace satisfactory to poor
    replace satisfactory to poor and splitting data at preprocessing step

--- code/loading_data.py
+++ code/loading_data.py
@@ -1,13 +1,13 @@
 import os
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import Optional
 
 dirname = os.path.dirname(__file__)
 
 class Category(StrEnum):
     GOOD = "good"
     POOR = "poor"
-    SATISFACTORY = "satisfactory"
     VERY_POOR = "very_poor"
 
 @dataclass
@@ -18,26 +18,41 @@ class Image:
 class LoadingType(StrEnum):
     RAW = "raw"
     PROCESSED = "processed"
+    SPLITTED = "splitted"
 
-def load_images(type: LoadingType) -> list[Image]:
+def load_images(type: LoadingType, split_name: Optional[str] = None) -> list[Image]:
     result: list[Image] = []
 
     folder_path = ""
     if type == LoadingType.RAW:
         folder_path = os.path.join(dirname, "../data_set/raw")
     elif type == LoadingType.PROCESSED:
-        folder_path = os.path.join(dirname, "../data_set/processed") 
+        folder_path = os.path.join(dirname, "../data_set/processed")
+    elif type == LoadingType.SPLITTED:
+        if split_name is None:
+            raise ValueError("split_name must be provided when loading splitted data")
+        folder_path = os.path.join(dirname, f"../data_set/splitted/{split_name}")
+    
     folder_path = os.path.abspath(folder_path) # remove ../ from path
 
     for subdir, _, files in os.walk(folder_path):
         if subdir == folder_path or subdir == "":
             continue
+        
+        category_str = os.path.basename(subdir)
+
+        category: Optional[Category] = None
+        if category_str == "satisfactory":
+            category = Category.POOR
+        elif category_str in list(map(lambda c: c.value, Category)):
+             category = Category(category_str)
+        else:
+            continue
 
         for file in files:
-            category = subdir.split(os.sep)[-1]
             result.append(Image(
                 image_path=os.path.join(subdir, file),
-                category=Category(category)
+                category=category
             ))
 
     return result
